96a062de419f17e31fa73a91adc8b2202070f958
RS256 <143985949+RS-256@users.noreply.github.com>
2026-04-07 16:10:21 +0900

    oh fixed publisher

--- build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("net.fabricmc.fabric-loom-remap")
-    // id("me.modmuss50.mod-publish-plugin")  // uncomment to enable publishing
-}
-if (sc.current.parsed >= "26.1") {
-    apply(rootProject.file("build.unobfuscated.gradle.kts"))
-} else {
-    apply(rootProject.file("build.obfuscated.gradle.kts"))
-}
